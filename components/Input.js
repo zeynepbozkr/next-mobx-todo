70a8027554f7d3946b7217d7fcfd0ad04fb0fa95
zeynepbozkr <73405507+zeynepbozkr@users.noreply.github.com>
2021-11-25 19:42:12 +0300

    add tabs

--- components/Input.js
+++ components/Input.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { observer } from "mobx-react-lite";
 import StoreProvider from "../utils/store-provider";
 import styles from "./Input.module.css";
-import { Button, Checkbox, Input, Modal } from "antd";
+import { Button, Checkbox, Input, Modal, Tabs } from "antd";
 import {
   EditOutlined,
   DeleteOutlined,
@@ -17,6 +17,7 @@ const InputTodos = () => {
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [modalTitle, setModalTitle] = useState(null);
   const [modalInput, setModalInput] = useState(null);
+  const [tabsKey, setTabsKey] = useState(2);
 
   const showModal = (title) => {
     setIsModalVisible(true);
@@ -29,8 +30,19 @@ const InputTodos = () => {
     setModalInput("");
   };
 
+  const { TabPane } = Tabs;
+
+  function callback(key) {
+    setTabsKey(key);
+  }
+
   return (
     <div>
+      <Tabs defaultActiveKey="1" onChange={callback}>
+        <TabPane tab="TODOS" key="1"></TabPane>
+        <TabPane tab="DONE" key="2"></TabPane>
+        <TabPane tab="TO DO" key="3"></TabPane>
+      </Tabs>
       <Modal
         title="Edit"
         visible={isModalVisible}
@@ -59,35 +71,93 @@ const InputTodos = () => {
             TodoStore.addTodo({ title: inputValue, isChecked: false });
             setInputValue("");
           }}
+          disabled={!inputValue}
         >
           ADD
         </Button>
       </div>
-      {TodoStore.todos?.map((item, index) => {
-        return (
-          <div className={styles.row}>
-            <Checkbox
-              checked={item.isChecked}
-              className={styles.checkbox}
-              onChange={(e) => TodoStore.checkTodo(item.title)}
-            ></Checkbox>
 
-            <div className={styles.list}> {item.title}</div>
-            <Button
-              className={styles.buttonEdit}
-              onClick={() => showModal(item.title)}
-            >
-              <EditOutlined />
-            </Button>
-            <Button
-              className={styles.buttonDelete}
-              onClick={() => TodoStore.removeAdd(item.title)}
-            >
-              <DeleteOutlined />
-            </Button>
-          </div>
-        );
-      })}
+      {tabsKey === "1"
+        ? TodoStore.todos?.map((item, index) => {
+            return (
+              <div className={styles.row}>
+                <Checkbox
+                  checked={item.isChecked}
+                  className={styles.checkbox}
+                  onChange={(e) => TodoStore.checkTodo(item.title)}
+                ></Checkbox>
+
+                <div className={styles.list}> {item.title}</div>
+                <Button
+                  className={styles.buttonEdit}
+                  onClick={() => showModal(item.title)}
+                >
+                  <EditOutlined />
+                </Button>
+                <Button
+                  className={styles.buttonDelete}
+                  onClick={() => TodoStore.removeAdd(item.title)}
+                >
+                  <DeleteOutlined />
+                </Button>
+              </div>
+            );
+          })
+        : tabsKey === "2"
+        ? TodoStore.todos?.map((item, index) => {
+            return (
+              item.isChecked && (
+                <div className={styles.row}>
+                  <Checkbox
+                    checked={item.isChecked}
+                    className={styles.checkbox}
+                    onChange={(e) => TodoStore.checkTodo(item.title)}
+                  ></Checkbox>
+
+                  <div className={styles.list}> {item.title}</div>
+                  <Button
+                    className={styles.buttonEdit}
+                    onClick={() => showModal(item.title)}
+                  >
+                    <EditOutlined />
+                  </Button>
+                  <Button
+                    className={styles.buttonDelete}
+                    onClick={() => TodoStore.removeAdd(item.title)}
+                  >
+                    <DeleteOutlined />
+                  </Button>
+                </div>
+              )
+            );
+          })
+        : TodoStore.todos?.map((item, index) => {
+            return (
+              !item.isChecked && (
+                <div className={styles.row}>
+                  <Checkbox
+                    checked={item.isChecked}
+                    className={styles.checkbox}
+                    onChange={(e) => TodoStore.checkTodo(item.title)}
+                  ></Checkbox>
+
+                  <div className={styles.list}> {item.title}</div>
+                  <Button
+                    className={styles.buttonEdit}
+                    onClick={() => showModal(item.title)}
+                  >
+                    <EditOutlined />
+                  </Button>
+                  <Button
+                    className={styles.buttonDelete}
+                    onClick={() => TodoStore.removeAdd(item.title)}
+                  >
+                    <DeleteOutlined />
+                  </Button>
+                </div>
+              )
+            );
+          })}
     </div>
   );
 };
